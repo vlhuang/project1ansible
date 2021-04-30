1372a13380c581a2a6e76a2483d251cb516eb970
vlhuang <74435342+vlhuang@users.noreply.github.com>
2021-04-30 03:56:04 +0000

    Add files via upload

---
+++ war-file/src/main/java/com/simplilearn/war/file/WarFileApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.war.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarFileApplication.class, args);
+	}
+
+}
